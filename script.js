79eee7f2e7e31040e209306f70b94e2d3350d344
delirium02 <darknightbs@gmail.com>
2025-12-19 20:59:07 +1100

    add book and display library functions

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-const Library = [];
+const libraryContainer = document.getElementById("library-container");
+
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
-    if(!new.target) {
-        throw Error("Book constructor must be called with 'new'");
-    }
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
 }
 
 Book.prototype.info = function () {
     const hasRead = this.read ? "Already read" : "Has not yet read";
-    return `${this.read}, by ${this.author}, ${this.pages} pages, ${this.read}`;
+    return `${this.title}, by ${this.author}, ${this.pages} pages, ${hasRead}`;
 };
+
+const addBookToLibrary = (book) => {
+    myLibrary.push(book);
+}
+
+const theGreatGatsby = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, false);
+addBookToLibrary(theGreatGatsby);
+
+
+function displayLibrary() {
+    myLibrary.forEach((book) => {
+        console.log(book.info());
+    });
+}
+
+displayLibrary();
+
+console.log(myLibrary[0].info());
